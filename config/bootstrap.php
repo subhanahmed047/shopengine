1dd195abd3ce07330251828b29456abcff22cb8b
Subhan Ahmed <aspcn47@gmail.com>
2019-06-16 20:47:57 +1000

    debug kit disable on PROD

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -194,10 +194,9 @@ Plugin::load('Migrations');
 
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
-//if (Configure::read('debug')) {
-    Configure::write('DebugKit', ['forceEnable' => true]);
+if (Configure::read('debug') && !IS_PROD) {
     Plugin::load('DebugKit', ['bootstrap' => true]);
-//}
+}
 
 /**
  * Connect middleware/dispatcher filters.
