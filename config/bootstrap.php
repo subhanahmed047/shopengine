11bc32f38e6d189cd9157fc53e0291dddf763231
Subhan Ahmed <aspcn47@gmail.com>
2019-06-16 20:43:07 +1000

    debug

--- config/bootstrap.php
+++ config/bootstrap.php
@@ -194,9 +194,9 @@ Plugin::load('Migrations');
 
 // Only try to load DebugKit in development mode
 // Debug Kit should not be installed on a production system
-if (Configure::read('debug')) {
+//if (Configure::read('debug')) {
     Plugin::load('DebugKit', ['bootstrap' => true]);
-}
+//}
 
 /**
  * Connect middleware/dispatcher filters.
